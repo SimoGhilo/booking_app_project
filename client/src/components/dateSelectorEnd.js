bc2e972e936993474b082ac5e6da3378ee6e3325
Simone Ghilotti <102835730+SimoGhilo@users.noreply.github.com>
2023-04-15 11:29:40 +0100

    update end date picker

--- client/src/components/dateSelectorEnd.js
+++ client/src/components/dateSelectorEnd.js
@@ -2,6 +2,9 @@ import React, { useEffect } from 'react';
 import '../styles/dateSelectors.css';
 
 const DateSelectorEnd = (props) => {
+    
+    let check_out_date = props.endDate.toLocaleDateString().split(/\//);
+    check_out_date = [check_out_date[2], check_out_date[1], check_out_date[0]].join('-');
 
 
     useEffect(() => {
@@ -9,7 +12,7 @@ const DateSelectorEnd = (props) => {
     }, [props.endDate, props.handleCheckOutDate])
     return (
         <div className='box'>
-            <input type="date" value={props.endDate.toISOString().slice(0, 10)} onChange={(e) => props.handleCheckOutDate(e.target.value)} min={props.tomorrow.toISOString().slice(0, 10)} />
+            <input type="date" value={check_out_date} onChange={(e) => props.handleCheckOutDate(e.target.value)} min={check_out_date} />
         </div>
     );
 };
