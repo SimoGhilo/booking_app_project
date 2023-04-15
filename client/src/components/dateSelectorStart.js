a5bacdeef5ef1196e6d13c8ff83aedb7856af6b0
Simone Ghilotti <102835730+SimoGhilo@users.noreply.github.com>
2023-04-15 11:28:35 +0100

    update picker start

--- client/src/components/dateSelectorStart.js
+++ client/src/components/dateSelectorStart.js
@@ -2,13 +2,16 @@ import React, { useEffect } from 'react';
 import '../styles/dateSelectors.css';
 
 const DateSelectorStart = (props) => {
+    
+    let check_in_date = props.startDate.toLocaleDateString().split(/\//);
+    check_in_date = [check_in_date[2], check_in_date[1], check_in_date[0]].join('-');
 
     useEffect(() => {
 
     }, [props.startDate, props.handleCheckInDate])
     return (
         <div className='box'>
-            <input type="date" value={props.startDate.toISOString().slice(0, 10)} onChange={(e) => props.handleCheckInDate(e.target.value)} min={props.todaysDate.toISOString().slice(0, 10)} />
+            <input type="date" value={check_in_date} onChange={(e) => props.handleCheckInDate(e.target.value)} min={check_in_date} />
         </div>
     );
 };
